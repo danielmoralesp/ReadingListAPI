46ce953ba1867238f347482cb59dc8e746125da3
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-02 09:30:48 -0500

    linsting_finished_book

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -10,7 +10,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     get '/books'
 
     assert_equal 200, response.status
-    assert_equal Mime::JSON, response.content_type
+    assert_equal Mime[:json], response.content_type
 
     assert_equal Book.count, json(response.body).size
   end
@@ -19,7 +19,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     get '/books?rating=5'
 
     assert_equal 200, response.status
-    assert_equal Mime::JSON, response.content_type
+    assert_equal Mime[:json], response.content_type
 
     assert_equal 1, json(response.body).size
   end
